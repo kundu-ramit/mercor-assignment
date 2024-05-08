a0f79f5063f5ab2f9450e4bce777d9331a8b0f60
kundu-ramit123 <140112358+kundu-ramit123@users.noreply.github.com>
2024-05-09 01:08:40 +0530

    .

--- backend/domain/query_processor/process_skills.go
+++ backend/domain/query_processor/process_skills.go
@@ -8,6 +8,8 @@ import (
 
 // ProcessExperience processes the experiences based on given items.
 func ProcessSkills(experiences []vectorrepository.Response) Result {
+	var optimScore float32 = 0.3
+	var topScoreMin float32 = 0.45
 	var result Result
 
 	// Sort experiences by score in descending order
@@ -16,11 +18,11 @@ func ProcessSkills(experiences []vectorrepository.Response) Result {
 	})
 
 	for _, experience := range experiences {
-		// If score is less than 0.3, ignore it
-		if experience.Score < 0.3 {
+		// If score is less than optimScore, ignore it
+		if experience.Score < optimScore {
 			continue
 		}
-		// If score is greater than or equal to 0.3, set IsPresent to true
+		// If score is greater than or equal to optimScore, set IsPresent to true
 		result.IsPresent = true
 		result.Responses = append(result.Responses, experience)
 	}
@@ -28,6 +30,9 @@ func ProcessSkills(experiences []vectorrepository.Response) Result {
 	// If no values remain, return the value with highest score and IsPresent is false
 	if len(result.Responses) == 0 && len(experiences) > 0 {
 		result.Responses = []vectorrepository.Response{{Text: experiences[0].Text, Score: experiences[0].Score}}
+	}
+	if result.Responses[0].Score < topScoreMin {
+		result.IsPresent = false
 	}
 
 	return result
